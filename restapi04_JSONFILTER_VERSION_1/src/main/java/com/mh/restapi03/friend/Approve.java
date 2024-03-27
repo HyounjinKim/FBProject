ea1b53e8ad6d38cda3acd30d8eb6ca41901a4f09
jungjc1117 <136683241+jungjc1117@users.noreply.github.com>
2024-03-27 14:44:06 +0900

    no message

--- restapi04_JSONFILTER_VERSION_1/src/main/java/com/mh/restapi03/friend/Approve.java
+++
@@ -1,6 +0,0 @@
-package com.mh.restapi03.friend;
-
-public enum Approve {
-
-    ING, Y, N
-}
--- restapi04_JSONFILTER_VERSION_1/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into friend (nickname, fnick, approve) values ('홍길동', '바나나', 1);
-insert into friend (nickname, fnick, approve) values ('김길동', '사과', 1);
-insert into friend (nickname, fnick, approve) values ('박길동', '딸기', 1);
