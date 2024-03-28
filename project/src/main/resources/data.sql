e86cfd4a26c4e3f08e25edf69a7f0912a97c9deb
김형진 <hyoung6658@gmail.com>
2024-03-28 12:12:03 +0900

    240328 - 세번째

---
+++ project/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into users (id,password,email,nickname,name,gender,birthdate,phonenumber,user_date,weight,height)
+values ('aaa','a123456!','aaa@naver.com','길동','일길동','MALE','240322','01012345678',now(),60,170);
